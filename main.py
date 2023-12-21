8685ae4415b2476e85a2c808dc7dc40ed87fa587
Your Name <you@example.com>
2023-12-21 13:13:27 +0200

    ali

---
+++ main.py
@@ -0,0 +1,20 @@
+a=int(input("first num:  "))
+b=int(input("second num: "))
+c=int(input("third num:  "))
+def num (a,b,c):
+ if a>b or a>c:
+    print(a)
+ elif b>a or b>c:
+    print(b)
+ elif c>a or c>b:
+    print(c)
+ else:
+    print("the numbers are equal")
+
+num (a,b,c)
+
+
+
+
+
+
